be624b39f396e17244b6d60a8b253cbe2360fef4
Kaynan Melo <kaynan.melo@gec.inatel.br>
2025-02-18 14:33:45 -0300

    Adionado txt para os comandos do git

--- main.cpp
+++ main.cpp
@@ -2,12 +2,15 @@
 
 using namespace std;
 
+
+
+
 int main()
 
 {
 	cout<<"1. Cadastrar cidade"<<endl<<"2. Cadastrar estrada"<<endl<<"3. Buscar centro Pokemon mais proximo"<<endl<<"4. Cadastrar Pokemon"<<endl<<"5. Remover Pokemon"<<endl<<"6. Listar Pokemon (Alfabetico)"<<endl<<"7. Listar Pokemon (Tipo)"<<endl<<"8. Contar Pokemon de cada tipo"<<endl<<"9. Encontrar Pokemon proximos"<<endl;
+
 	
-	cout<<"Hello World!"<<endl;
 	
 	return 0;
 }
