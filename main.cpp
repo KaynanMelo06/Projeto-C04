d4310151e33824f4cfcc51e26c2d6140cb9be98c
D4LM0 <dalmoneto0405@gmail.com>
2025-02-18 14:26:46 -0300

    Adiciondo main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	cout<<"1. Cadastrar cidade"<<endl<<"2. Cadastrar estrada"<<endl<<"3. Buscar centro Pokemon mais proximo"<<endl<<"4. Cadastrar Pokemon"<<endl<<"5. Remover Pokemon"<<endl<<"6. Listar Pokemon (Alfabetico)"<<endl<<"7. Listar Pokemon (Tipo)"<<endl<<"8. Contar Pokemon de cada tipo"<<endl<<"9. Encontrar Pokemon proximos"<<endl;
+	
+	return 0;
+}
