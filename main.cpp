c528393841b8198f43f2b66399c5c0249bdfc7c3
Kaynan Melo <kaynan.melo@gec.inatel.br>
2025-02-25 16:32:28 -0300

    Dei uma embelezada no codigo

--- main.cpp
+++ main.cpp
@@ -1,67 +1,77 @@
 #include <iostream>
+#include <cstdlib> // Biblioteca utilizada para limpar o terminal, com o comando system("CLS");
 
 using namespace std;
 
 void cadastrarCidade()
 {
+	cout << "cadastrar cidade" << endl;
 	cout << "------------------------------" << endl;
-	cout << "*Funcionalidade em construcao*" << endl;
+	cout << "*Funcionalidade em construcao*  RETORNANDO PARA O MENU" << endl;
 	cout << "------------------------------" << endl;
 }
 
 void cadastrarEstrada()
 {
+	cout << "cadastrar estrada" << endl;
 	cout << "------------------------------" << endl;
-	cout << "*Funcionalidade em construcao*" << endl;
+	cout << "*Funcionalidade em construcao*  RETORNANDO PARA O MENU" << endl;
 	cout << "------------------------------" << endl;
 }
 
 void buscarCentroPokemonMaisProximo()
 {
+	cout << "buscar centro pokemon mais proximo" << endl;
 	cout << "------------------------------" << endl;
-	cout << "*Funcionalidade em construcao*" << endl;
+	cout << "*Funcionalidade em construcao*  RETORNANDO PARA O MENU" << endl;
 	cout << "------------------------------" << endl;
 }
 
 void cadastrarPokemon()
 {
+	cout << "cadastrar pokemon" << endl;
 	cout << "------------------------------" << endl;
-	cout << "*Funcionalidade em construcao*" << endl;
+	cout << "*Funcionalidade em construcao*  RETORNANDO PARA O MENU" << endl;
 	cout << "------------------------------" << endl;
 }
 
 void removerPokemon()
 {
+	cout << "remover pokemon" << endl;
 	cout << "------------------------------" << endl;
-	cout << "*Funcionalidade em construcao*" << endl;
+	cout << "*Funcionalidade em construcao*  RETORNANDO PARA O MENU" << endl;
 	cout << "------------------------------" << endl;
 }
 
 void listarPokemonAlfabetico()
 {
+	cout << "listar pokemon alfabetico" << endl;
 	cout << "------------------------------" << endl;
-	cout << "*Funcionalidade em construcao*" << endl;
+	cout << "*Funcionalidade em construcao*  RETORNANDO PARA O MENU" << endl;
 	cout << "------------------------------" << endl;
 }
 
 void listarPokemonTipo()
 {
+	cout << "listar pokemon tipo" << endl;
 	cout << "------------------------------" << endl;
-	cout << "*Funcionalidade em construcao*" << endl;
+	cout << "*Funcionalidade em construcao*  RETORNANDO PARA O MENU" << endl;
 	cout << "------------------------------" << endl;
 }
 
 void contarPokemonTipo()
 {
+	cout << "contar pokemon tipo" << endl;
 	cout << "------------------------------" << endl;
-	cout << "*Funcionalidade em construcao*" << endl;
+	cout << "*Funcionalidade em construcao*  RETORNANDO PARA O MENU" << endl;
 	cout << "------------------------------" << endl;
 }
 
 void encontrarPokemonProximos()
 {
+	cout << "encontrar pokemon proximos" << endl;
 	cout << "------------------------------" << endl;
-	cout << "*Funcionalidade em construcao*" << endl;
+	cout << "*Funcionalidade em construcao*  RETORNANDO PARA O MENU" << endl;
 	cout << "------------------------------" << endl;
 }
 
@@ -71,9 +81,8 @@ int main()
 
 	do
 	{
-		cout << endl;
-		cout << "Para teste, coloquei um loop infinito" << endl << endl;
-		cout << "Pokedex" << endl
+
+		cout << "Pokedex:" << endl
 			 << endl
 			 << "1. Cadastrar cidade" << endl
 			 << "2. Cadastrar estrada" << endl
@@ -83,7 +92,8 @@ int main()
 			 << "6. Listar Pokemon (Alfabetico)" << endl
 			 << "7. Listar Pokemon (Tipo)" << endl
 			 << "8. Contar Pokemon de cada tipo" << endl
-			 << "9. Encontrar Pokemon proximos" << endl;
+			 << "9. Encontrar Pokemon proximos" << endl
+			 << "10. Fechar Menu" << endl;
 
 		int opcao;
 
@@ -92,39 +102,51 @@ int main()
 		switch (opcao)
 		{
 		case 1:
+			system("CLS");
 			cadastrarCidade();
 			break;
 		case 2:
+			system("CLS");
 			cadastrarEstrada();
 			break;
 		case 3:
+			system("CLS");
 			buscarCentroPokemonMaisProximo();
 			break;
 		case 4:
+			system("CLS");
 			cadastrarPokemon();
 			break;
 		case 5:
+			system("CLS");
 			removerPokemon();
 			break;
 		case 6:
+			system("CLS");
 			listarPokemonAlfabetico();
 			break;
 		case 7:
+			system("CLS");
 			listarPokemonTipo();
 			break;
 		case 8:
+			system("CLS");
 			contarPokemonTipo();
 			break;
 		case 9:
+			system("CLS");
 			encontrarPokemonProximos();
 			break;
-		default:
-			cout << "Opcao invalida" << endl;
+		case 10:
+			system("CLS");
+			cout << "Menu fechando...";
 			acabar = true;
+			break;
+		default:
+			cout << "Opcao invalida, escolha novamente a opcao" << endl;
 			break;
 		}
 
-		
 	} while (acabar == false);
 
 	return 0;
