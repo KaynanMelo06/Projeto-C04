e14bd263d2c159e2c095c7dfc95a85c3c1d1f2d9
Kaynan Melo <kaynan.melo@gec.inatel.br>
2025-03-19 17:44:58 -0300

    adicionado a pasta c05

--- Moodle C04/Busca Binaria++.cpp
+++ Moodle C04/Busca Binaria++.cpp
@@ -1,59 +1,79 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+
 using namespace std;
 
-int binaria (int vetor[], int tamanho, int x)
+// Função de busca binária recursiva
+int buscaPoneglyph(string ilhas[], int esquerda, int direita, string pista)
 {
-	bool achou;
-	int baixo, meio, alto;
-	
-	baixo = 0;
-	alto = tamanho -1;
-	achou = false;
-	
-	while((baixo <= alto) && (achou == false))
-	{
-	
-		meio = (baixo + alto) / 2;
-		if(x <vetor[meio])
-		{
-			alto = meio -1;
-		}
-		else 
-		{
-			if(x > vetor[meio])
-			{
-				baixo = meio + 1;
-			}
-			else
-			{
-				achou = true;
-			}
-		}
-	}
-	
-	if(achou == true)
-	{
-		return meio;
-	}
-	else
-	{
-		return -1;
-	}
+    if (esquerda <= direita)
+    {
+        int meio = esquerda + (direita - esquerda) / 2;
+
+        // Se a pista for encontrada, retorna o índice
+        if (ilhas[meio] == pista)
+        {
+            return meio;
+        }
+       
+        // Se a pista for menor, busca na metade esquerda
+        if (ilhas[meio] > pista)
+        {
+            return buscaPoneglyph(ilhas, esquerda, meio - 1, pista);
+        }
+
+        // Se a pista for maior, busca na metade direita
+        else
+        {
+            return buscaPoneglyph(ilhas, meio + 1, direita, pista);
+        }
+    }
+    return -1; // Pista não encontrada
 }
 
 int main()
 {
-	int vetor[10], n, pos;
+    const int tam_max = 100;
+    string ilhas[tam_max];
+    int n = 0;
+    string nome;
 
-	cin >> n;
-	
-	for(int i = 0; i < n; i++)
-	{
-		cin >> vetor[i];
-	}
-	
-	cin >> pos;
+    while (true){
 
-	cout << binaria(vetor, n, pos) << endl;
-	return 0;
+        getline(cin, nome);
+        bool repetido = false;
+        for (int i = 0; i < n; i++)
+        {
+            if (ilhas[i] == nome)
+            {
+                repetido = true;
+                break;
+            }
+        }
+        if (repetido)
+        {
+            break;
+        }
+        ilhas[n++] = nome;
+
+    }
+    
+
+    // Última ilha digitada é a pista a ser buscada
+    string pista = nome;
+
+    // Chama a função de busca binária 0 = inicio do vetor, n - 2 = final do vetor
+    int indice = buscaPoneglyph(ilhas, 0, n - 2, pista);
+
+    // Exibe o resultado
+    if (indice != -1)
+    {
+        cout << "Índice da pista encontrada: " << indice << endl;
+    }
+    else
+    {
+        cout << "Poneglyph não encontrada." << endl;
+    }
+
+    return 0;
 }
